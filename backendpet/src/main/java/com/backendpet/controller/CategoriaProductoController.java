6d09fd7b1bc7303fb7a71bbc2d3770b1eb8dd49f
Benjisus3007 <benjamin.aedo3007@gmail.com>
2026-05-27 08:36:15 -0400

    Especificación de origin en COR para evitar problemas con Angular

--- backendpet/src/main/java/com/backendpet/controller/CategoriaProductoController.java
+++ backendpet/src/main/java/com/backendpet/controller/CategoriaProductoController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController 
 @RequestMapping("/api/categorias") 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CategoriaProductoController {
 
     @Autowired
